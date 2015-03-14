220394323436be02a2a674a0efba80b67a4953e4
showc7 <showc7@gmail.com>
2015-03-14 13:49:01 +0300

    add station parameters

--- scripts/conf.js
+++ scripts/conf.js
@@ -5,5 +5,23 @@ APP.conf = {
     cellSize: 10,
 
     // max station signal length in miters
-    maxStationSignalLength: 100
+    maxStationSignalLength: 100,
+
+    // powero f transmitter in watts
+    powerOfTransmitter: 20,
+
+    // gain of the receiving antenna
+    receivingAntennaGain: 1,
+
+    // gain of the transmitting antenna
+    transmittingAntennaGain: 1,
+
+    // looses in receiving antenna
+    lossesInReceivingAntenna: 0.1,
+
+    // looses in transmitting antenna
+    loosesInTransmittingAntenna: 0.1,
+
+    // station wave length
+    waveLength: 0.15
 };
