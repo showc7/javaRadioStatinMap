5ddd28725b3833b967926f0eb6019d71f09611e9
showc7 <showc7@gmail.com>
2015-03-14 14:36:21 +0300

    update calculate

--- scripts/core.js
+++ scripts/core.js
@@ -10,9 +10,45 @@ Core.prototype.convertPixelsToMiters = function() {
 Core.prototype.initialize = function() {
 	
 };
+// return array of cells
+Core.prototype.calculate = function(point) {
+	console.log(point);
+	var resultCells = [];
 
-Core.prototype.calculate = function() {
-	
+	// station place in cells X
+	var stationX = 4;
+	// station place in cells Y
+	var stationY = 4;
+
+	// distance between two points
+	var Radius = 0;
+
+	var width = APP.conf.maxStationSignalLength / APP.conf.cellSize + 4;
+	var heigth = width;
+
+	for(var i = 0; i < width; i++) {
+		for(var j = 0; j < heigth; j++) {
+			var radius = Math.sqrt(Math.pow(Math.abs(stationX - i),2) + Math.pow(Math.abs(stationY - j),2));
+			resultCells[i + j * width].opacity = calculateOpacity(radius);
+
+		}
+	}
+
+	return resultCells;
+};
+
+Core.prototype.calculateOpacity = function(radius) {
+	return	APP.conf.powerOfTransmitter +
+			APP.conf.receivingAntennaGain +
+			APP.conf.transmittingAntennaGain -
+			(Math.pow(4 * Math.PI * radius / APP.conf.waveLength,2) +
+			APP.conf.lossesInReceivingAntenna +
+			APP.conf.loosesInTransmittingAntenna) +
+			freeSpacePowerLoose(radius) / APP.conf.transmittingAntennaGain;
+};
+
+Core.prototype.freeSpacePowerLoose = function(radius) {
+	return 0.1;
 };
 
 // point in pixels
