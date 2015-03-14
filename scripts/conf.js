373ef82d0634c54cb64eb5a663b4eea5fc4b06c8
Edvards Kazlouski <edward2769940@gmail.com>
2015-03-14 13:14:16 +0300

    Change config

--- scripts/conf.js
+++ scripts/conf.js
@@ -2,9 +2,9 @@ var APP = {};
 
 APP.conf = {
     defaultMapLocation: new Point(53.902, 27.556),
-    
-    // size of visibility square in pixels
-    visibilitySquareSize: 20,
+
+    // size of visibility square in mitres
+    cellSize: 10,
 
     // max station signal length in miters
     maxStationSignalLength: 100
